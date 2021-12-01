f9ba33f39e7e78b1d00d78a1d00e6a1d4994c589
eriklemy <eriklemy2010@gmail.com>
2021-12-01 10:56:20 -0300

    day 1 complete (simplify)

--- day_1/sonarSweep.cpp
+++ day_1/sonarSweep.cpp
@@ -28,11 +28,8 @@ int main() {
 
 int sonarSweepPartOne(vector<int> &depth) {
     size_t count = 0;
-    size_t auxMax = 0;
     for(size_t i = 0; i < depth.size() - 1; i++) {
-        auxMax = depth[i];
-        if(depth[i+1] > auxMax) {
-            auxMax = depth[i+1];
+        if(depth[i+1] > depth[i]) {
             count++;
         }
     }
@@ -44,7 +41,7 @@ int sonarSweepPartTwo(vector<int> &depth) {
     size_t auxMax = 0;
     for(size_t i = 0; i < depth.size() - 2; i++) {
 		auxMax = depth[i] + depth[i+1] + depth[i+2];
-		if((depth[i+1] + depth[i+2] + depth[i+3]) > auxMax) {
+		if((depth[i+1] + depth[i+2] + depth[i+3]) > (depth[i] + depth[i+1] + depth[i+2])) {
 			auxMax = depth[i+1] + depth[i+2] + depth[i+3];
 			count++;
 		}
