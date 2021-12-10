c67c91e2a3b92112c21c20d756a49dda87692d53
eriklemy <eriklemy2010@gmail.com>
2021-12-10 20:16:17 -0300

    day 10 completed

---
+++ day_10/SyntaxScoring.py
@@ -0,0 +1,44 @@
+SYNTAXCHECK = {'(': ')', '[': ']', '{': '}', '<': '>'}
+SYNTAXOPEN = {'(': 1, '[': 2, '{': 3, '<': 4} # autocomplete {')': 1, ']': 2, '}': 3, '>': 4}
+SYNTAXCLOSE = {')': 3, ']': 57, '}': 1197, '>': 25137}
+
+def points(value: str) -> int:
+    return SYNTAXCLOSE[value]
+    
+def checkSyntaxPartOneTwo(data: list) -> int:
+    count = 0
+    autocomplete = []
+    for line in data:
+        stack = []
+        corrupted = False
+        for value in line:
+            if value in SYNTAXCHECK:
+                stack.append(value)
+            else:
+                if value not in SYNTAXCHECK:
+                    top = stack[-1]
+                    stack.pop()
+                    if SYNTAXCHECK[top] != value:
+                        count += points(value)
+                        corrupted = True
+                        break
+        if not corrupted:  
+            errorsCount = 0
+            while stack:
+                errorsCount = errorsCount * 5 + SYNTAXOPEN[stack.pop()]
+            autocomplete.append(errorsCount)
+
+    autocomplete.sort()
+    autocompleteScore = autocomplete[len(autocomplete)//2]
+    return count, autocompleteScore
+
+def main() -> None:
+    with open("day_10/input.txt") as file:
+        data = [[value for value in list(line)] for line in file.read().strip().split('\n')]
+
+    part_1, part_2 = checkSyntaxPartOneTwo(data)
+    print(f'Result <PartOne>: {part_1}')
+    print(f'Result <PartTwo>: {part_2}')
+
+if __name__ == '__main__' :
+    main()
